22a8f5574abfeb91310c42422247ca8da784bf45
Inbal Schkolnik <capoeira0012>
2020-02-12 19:35:13 +0200

    password size validation func added

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import random
 
 
+def password_size():
+    size = 0
+    while True:
+        try:
+            size = int(input("Enter password size: "))
+            if size < 6:
+                print("Password size must be 6 chars min")
+            else:
+                return size
+        except:
+            print("Please enter a number")
+
+
 chars = "@#$%123456abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 password = ""
-pass_size = int(input("Enter password size: "))
+pass_size = password_size()
 
 for char in range(0, pass_size):
     char = chars[random.randrange(0, len(chars))]
